60f4b649a3674956ca8e5075b98c7e5645d3a6be
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-11-24 21:47:30 +0000

    Added GitHub profile and repository links

--- src/App.js
+++ src/App.js
@@ -3,5 +3,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/eknox22/" target="_blank" rel="noreferrer">
+          {" "}
+          Emily Knox-Clifton
+        </a>
+        {""} and is {""}
+        <a
+          href="https://github.com/eknox22/React-Weather-Application"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
