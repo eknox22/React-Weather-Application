a9b713fd96b17a41afe2d82737fcfe0cdb6f578e
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-11-25 18:57:59 +0000

    Installed Bootstrap
    yarn install bootstrap
    
    import "bootstrap/dist/css/bootstrap.css";

--- src/App.js
+++ src/App.js
@@ -5,31 +5,37 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/eknox22/" target="_blank" rel="noreferrer">
-          {" "}
-          Emily Knox-Clifton
-        </a>
-        {""} and is {""}
-        <a
-          href="https://github.com/eknox22/React-Weather-Application"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub{" "}
-        </a>
-        and {""}
-        <a
-          href="https://merry-gnome-6832f9.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/eknox22/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Emily Knox-Clifton
+          </a>
+          {""} and is {""}
+          <a
+            href="https://github.com/eknox22/React-Weather-Application"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+          and {""}
+          <a
+            href="https://merry-gnome-6832f9.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
