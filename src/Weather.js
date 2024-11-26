78b079f01b48c011e51a0bc363aade44d609767b
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-11-26 20:21:08 +0000

    Initial import of data from api

--- src/Weather.js
+++ src/Weather.js
@@ -1,59 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  const apiKey = "t61a3de96178c340b47o7ff32962bb7f";
-  let city = "Bristol";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}`;
-  return (
-    <div className="weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              placeholder="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Bristol</h1>
-      <ul>
-        <li>Monday 19:45</li>
-        <li>Mostly cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="placeholder"
-              className="float-left"
-            />
-            <div className="float-left">
-              <span className="temperature">6</span>
-              <span className="unit">°C</span>
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                placeholder="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Preciptation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind : 13 km/h</li>
-          </ul>
+        </form>
+        <h1>Bristol</h1>
+        <ul>
+          <li>Monday 19:45</li>
+          <li>Mostly cloudy</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="placeholder"
+                className="float-left"
+              />
+              <div className="float-left">
+                <span className="temperature">{Math.round(temperature)}</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Preciptation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind : 13 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "t61a3de96178c340b47o7ff32962bb7f";
+    let city = "Bristol";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
