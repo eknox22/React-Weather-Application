25483efaa7b1c9e91dc5ec1b00c75437e3f265f5
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-11-26 21:52:33 +0000

    Moving the city inside a property

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -73,8 +73,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "t61a3de96178c340b47o7ff32962bb7f";
-    let city = "Bristol";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.defaultCity}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
